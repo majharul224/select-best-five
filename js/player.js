e1c8827bbb6596a76f5f27c9a6e59fdeb16987d4
majharul224 <majharulislam2225@gmail.com>
2022-08-22 22:48:49 +0600

    added a function

--- js/player.js
+++ js/player.js
@@ -1,3 +1,25 @@
-function addToPlayer(){
-    
+const playerArray = [];
+function display(){
+// console.log(cardPlayersName)
+if(playerArray.length <=5){
+    const oderList = document.getElementById("player-list")
+    const li = document.createElement("li")
+    for( let i = 0; i<playerArray.length ; i++){
+        li.innerText = `${playerArray[i]}`
+        }
+        oderList.appendChild(li)
+}
+
+else{
+    alert('full')
+}
+}
+
+function addToPlayer(btn){
+    const playerName = (btn.parentNode.children[0].innerText);
+    // console.log(playerName)
+    playerArray.push(playerName)
+    display()
+    btn.disabled = 'true'
+     console.log(playerArray)
 }
