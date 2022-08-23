4dc98c78218964970e89abb8195b93187d2be58a
majharul224 <majharulislam2225@gmail.com>
2022-08-23 17:52:27 +0600

    add function coach

--- js/totalBalance.js
+++ js/totalBalance.js
@@ -1,26 +1,27 @@
 document.getElementById("total-calculate").addEventListener('click', function() {
+    // maneger field
     const managerBalnceField = document.getElementById("manager-field")
     const managerBalnceString = managerBalnceField.value
     const managerBalnce = parseFloat(managerBalnceString)
     managerBalnceField.value = " "
 
     // corse field
-    /* 
-        const coachBalnceField = document.getElementById("coach-field")
-        const coachBalnceString = coachBalnceField.value
-        const coachBalnce = parseFloat(coachBalnceString);
 
-        if (Number.isNaN(managerBalnce) == true || Number.isNaN(coachBalnce) == true) {
-            alert("please input number in manager and coach")
-        }
-     */
-    /* 
-        coachBalnceField.value = " "
-        const playerExpenses = document.getElementById("player-expenses").innerHTML;
+    const coachBalnceField = document.getElementById("coach-field")
+    const coachBalnceString = coachBalnceField.value
+    const coachBalnce = parseFloat(coachBalnceString);
 
-        // total calcoleter
+    if (Number.isNaN(managerBalnce) == true || Number.isNaN(coachBalnce) == true) {
+        alert("please input number in manager and coach")
+    }
 
-        const totalBalance = parseFloat(playerExpenses) - (parseFloat(managerBalnceString) + parseFloat(coachBalnceString));
-        document.getElementById("total").innerHTML = totalBalance
-     */
+
+    coachBalnceField.value = " "
+    const playerExpenses = document.getElementById("player-expenses").innerHTML;
+
+    // total calcoleter
+
+    const totalBalance = parseFloat(playerExpenses) - (parseFloat(managerBalnceString) + parseFloat(coachBalnceString));
+    document.getElementById("total").innerHTML = totalBalance
+
 })
